98166f004f3bf8b883326b1c2aaeefe60a324cee
wenweiguang <172051826@qq.com>
2020-06-11 16:01:01 +0800

    剪裁布局

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'basicwidget/container_test.dart';
 import 'basicwidget/scaffold_test.dart';
+import 'basicwidget/clip_test.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'flutter study',
     debugShowCheckedModeBanner: false,
-    home: ScaffoldWTest(),
+    home: ClipWTest(),
   ));
 }
